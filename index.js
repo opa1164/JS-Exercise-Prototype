3c425d157202ee141eb3a7781403394273196180
Ian Reed <opa1164@yahoo.com>
2021-01-27 18:01:26 -0600

    Create a CodeGrade submission

--- index.js
+++ index.js
@@ -39,11 +39,27 @@ function Airplane(name) {
           + It should return a string with `name` and `age`. Example: "Mary, 50"
   */
   
- function Person() {
-    
+ function Person(name, age) 
+  {
+    this.name = name;
+    this.age = age;
+    this.stomach = [];
+    this.toString = function(){
+      return `${name}, ${age}`;
+    }
   }
  
- 
+  Person.prototype.eat = function(edible){
+  if(this.stomach.length < 10)
+      {
+        this.stomach.push(edible);
+      }
+ }
+
+ Person.prototype.poop = function(){
+  this.stomach = [];
+  console.log(this.stomach);
+ }
 
   
   
@@ -63,8 +79,14 @@ function Airplane(name) {
           + The `drive` method should return a string "I ran out of fuel at x miles!" x being `odometer`.
   */
   
- function Car() {
-    
+ function Car(model, milesPerGallon) {
+    this.model = model;
+    this.milesPerGallon = milesPerGallon;
+    this.tank = 0;
+    this.odometer = 0;
+  }
+  Car.prototype.fill = function(gallons){
+    this.tank += gallons;
   }
   
   
@@ -75,18 +97,22 @@ function Airplane(name) {
       - Besides the methods on Person.prototype, babies have the ability to `.play()`:
           + Should return a string "Playing with x", x being the favorite toy.
   */
- function Baby() {
-   
+ function Baby(name, age, favToy) {
+   Person.call(this, name, age);
+   this.favoriteToy = favToy;
   }
- 
+  Baby.prototype = Object.create(Person.prototype);
+ Baby.prototype.play = function(){
+   return `Playing with ${this.favoriteToy}`;
+ }
   
   /* 
     TASK 4
     In your own words explain the four principles for the "this" keyword below:
-    1. 
-    2. 
-    3. 
-    4. 
+    1. Window Binding- in global scope 'this' refers to the console object
+    2. Implicit Binding - this refers to the object 
+    3. New binding - refers to the object just created
+    4. Explicit binding - used in .call and .apply
   */
   
   
